8c26ac8baa8da70977394d6641fbc226e71f805b
Ostap12 <ostapbor@gmail.com>
2017-07-04 22:26:49 +0300

    added gallery page

--- js/app.js
+++ js/app.js
@@ -1,27 +1,32 @@
 
-function initialize() {
-    var mapProp = {
-        center: new google.maps.LatLng(50.464379, 30.519131),
-        zoom: 11
-    };
-    var html_element = document.getElementById("googleMap");
-    var map = new google.maps.Map(html_element, mapProp);
-
-    var point = new google.maps.LatLng(50.464379,30.519131);
-    var contentString = '<b>' +'Top Hotel Kyiv' + '</b>';
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
+var main = function () {
+    $('.gallery1').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        cssEase: 'ease-in',
+        slidesToShow: 4,
+       asNavFor: '.gallery2'
     });
 
-    var marker = new google.maps.Marker({
-        position: point,
-        //map - це змінна карти створена за допомогою new google.maps.Map(...)
-        map:  map
-    });
+    $('.gallery2').slick({
+        dots: false,
+        slidesToShow: 7,
+        centerMode: true,
+        centerPadding: '40px',
+        asNavFor: '.gallery1',
+        focusOnSelect: true,
+        arrows: false,
+        responsive: [{
 
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3
+            }
+
+        }]
+    })
+};
+
+$(document).ready(main);
+
 
---
+++ js/maps.js
@@ -0,0 +1,26 @@
+function initialize() {
+    var mapProp = {
+        center: new google.maps.LatLng(50.464379, 30.519131),
+        zoom: 11
+    };
+    var html_element = document.getElementById("googleMap");
+    var map = new google.maps.Map(html_element, mapProp);
+
+    var point = new google.maps.LatLng(50.464379,30.519131);
+    var contentString = '<b>' +'Top Hotel Kyiv' + '</b>';
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+        position: point,
+        //map - це змінна карти створена за допомогою new google.maps.Map(...)
+        map:  map,
+        icon: "../img/home-icon.png"
+    });
+
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
