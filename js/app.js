b6c20ab69eca9108ff23ed57c2ee49e864153251
Ostap12 <ostapbor@gmail.com>
2017-07-08 19:49:25 +0300

    added booking form

--- js/app.js
+++ js/app.js
@@ -1,6 +1,93 @@
 
 var main = function () {
+    $(function () {
+        var dialog, form,
 
+            emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+            name = $("#name"),
+            email = $("#email"),
+            phone = $("#phone"),
+            allFields = $([]).add(name).add(email).add(phone),
+            tips = $(".validateTips");
+
+        function updateTips(t) {
+            tips
+                .text(t)
+                .addClass("ui-state-highlight");
+            setTimeout(function () {
+                tips.removeClass("ui-state-highlight", 1500);
+            }, 500);
+        }
+
+        function checkLength(o, n, min, max) {
+            if (o.val().length > max || o.val().length < min) {
+                o.addClass("ui-state-error");
+                updateTips("Довжинна " + n + " повинна бути між " +
+                    min + " і " + max + ".");
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        function checkRegexp(o, regexp, n) {
+            if (!( regexp.test(o.val()) )) {
+                o.addClass("ui-state-error");
+                updateTips(n);
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        function addUser() {
+            var valid = true;
+            allFields.removeClass("ui-state-error");
+
+            valid = valid && checkLength(name, "username", 3, 16);
+            valid = valid && checkLength(email, "email", 6, 80);
+            valid = valid && checkLength(phone, "phone", 10, 10);
+
+            valid = valid && checkRegexp(name, /^[a-z]([0-9a-z_\s])+$/i, "Мінімальна довжина імені - 3 символи");
+            valid = valid && checkRegexp(email, emailRegex, "приклад: ostap@tophotel.com");
+            valid = valid && checkRegexp(phone, /^([0-9])+$/, "Тільки цифри");
+
+            if (valid) {
+                $("#users tbody").append("<tr>" +
+                    "<td>" + name.val() + "</td>" +
+                    "<td>" + email.val() + "</td>" +
+                    "</tr>");
+                dialog.dialog("close");
+            }
+            return valid;
+        }
+
+        dialog = $("#dialog-form").dialog({
+            autoOpen: false,
+            height: 400,
+            width: 350,
+            modal: true,
+            buttons: {
+                "Забронювати": addUser,
+                Скаcувати: function () {
+                    dialog.dialog("close");
+                }
+            },
+            close: function () {
+                form[0].reset();
+                allFields.removeClass("ui-state-error");
+            }
+        });
+
+        form = dialog.find("form").on("submit", function (event) {
+            event.preventDefault();
+            addUser();
+        });
+
+        $("#book").button().on("click", function () {
+            dialog.dialog("open");
+        });
+    });
 
     $('.gallery1').slick({
         autoplay: true,
@@ -39,6 +126,10 @@ var main = function () {
         pauseOnFocus: false,
         pauseOnHover: false
     });
+
+
+
+
 
 
 
